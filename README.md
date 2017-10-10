5d6e3a903420a8fcf1d917cef3fa998cba03efef
rasca0027 <rasca0027@gmail.com>
2017-10-10 11:51:34 -0400

    test

---
+++ README.md
@@ -0,0 +1,22 @@
+Lorem Ipsum Zine
+---
+
+
+This repo is the site for the first issue of Lorem Ipsum.
+It is based on Express.js and EJS template engine.
+
+Follow us:
+- [Main site](http://bxmc.poly.edu/LoremIpsum/)
+- [Facebook](https://www.facebook.com/loremipsumzine/)
+- [Twitter](https://twitter.com/search?q=%40loremipsum_zine)
+- [Instagram](https://www.instagram.com/loremipsum_zine/)
+
+
+TODO:
+- index
+- different image handle
+- logo
+- page num
+- issue num
+- static file/image
+- catalog (optional)
---
+++ index.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var Parse = require('parse/node');
+var fs = require("fs");
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// index
+var index = [];
+var tagsColorTable = {
+    'ENTER': '#f4764a',
+    '360 Video': '#AF1FFF'
+};
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(request, response) {
+    response.render('pages/index');
+});
+
+
+app.get('/editors', function(request, response) {
+    response.render('pages/editor', {'title': "Editor's Note"});
+});
+
+
+app.get('/project/:id', function(request, response) {
+    var id = request.params.id; 
+    var content = fs.readFileSync("data/project" + id + ".json");
+    content = JSON.parse(content);
+    
+    var tag1 = content.tag[0];
+    var color1 = tagsColorTable[tag1];
+    var tag2 = content.tag[1];
+    var color2 = tagsColorTable[tag2];
+
+    var context = {
+        'title': content.title,
+        'author': content.author,
+        'authorLink': content.authorLink,
+        'info': content.info,
+        'description': content.content,
+        'types': content.type,
+        'link': content.link,
+        'tag1': tag1,
+        'tag1color': color1,
+        'tag2': tag2,
+        'tag2color': color2,
+        'next': parseInt(request.params.id) + 1,
+        'prev': parseInt(request.params.id) - 1
+    };
+    response.render('pages/project', context);
+});
+
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
